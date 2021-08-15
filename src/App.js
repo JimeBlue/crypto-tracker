30e00bd88c2c73ad6a1cab7aa19d1e5c3c100426
Jimena <jimeblue69@gmail.com>
2021-08-15 14:37:23 +0200

    Install axios, add useEffect and form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+
+
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+      )
+      .then(res => {
+        setCoins(res.data);
+      })
+      .catch(error => alert(error));
+  }, []);
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="coin-app"> 
+      <div className='coin-search'>
+      <h1 className='coin-text'>Search a currency</h1>
+      <form>
+          <input
+            className='coin-input'
+            type='text'
+            placeholder='Search'
+          />
+        </form>
+      </div>
     </div>
   );
 }
